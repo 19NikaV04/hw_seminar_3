c5e2ce9ace509ec3302c80f3b82e03c80f411cba
Veronika <Vasileva.s.veronika@gmail.com>
2023-12-14 20:56:13 +0300

    Добавила решение 1 задачи

---
+++ task 001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1
+
+Console.Write("Введите значение M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void PrintNambers(int M, int N)
+{
+    if(M > N) return;
+    Console.Write(M + " ");
+    PrintNambers(M + 1, N);
+}
+
+PrintNambers(M, N);
