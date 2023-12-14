87c07ebbccc10965050bc3aa1051d47fbde7536a
Veronika <Vasileva.s.veronika@gmail.com>
2023-12-14 21:13:41 +0300

    Добавила решение 3 задачи

---
+++ task 003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3
+
+int[] numbers = {1,2,3,4,5,6,7,8,9};
+
+void ConsoleWrite(int currentindex)
+{
+if (currentindex >= 1)
+{
+Console.WriteLine(numbers[currentindex - 1]);
+ConsoleWrite(--currentindex);
+}
+}
+
+ConsoleWrite(numbers.Length);
